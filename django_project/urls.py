0b708df35a6cc584a8f10c202d0c2e6dc0ae64a5
Mirabror <fayzullayevmirabror2@gmail.com>
2023-11-19 23:06:43 +0500

    all folders are renamed

---
+++ django_project/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from registration import views as rv
+from django.conf import settings
+from django.conf.urls.static import static 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("chat.urls")),
+    path('signup/', rv.SignUp, name="register"),
+    path("", include("django.contrib.auth.urls")),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
